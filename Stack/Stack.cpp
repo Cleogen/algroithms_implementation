f733cee7612fbb4dab73b94fd393ea30744eb26f
ishkhan <ishkhan.n23@gmail.com>
2018-09-22 12:57:49 +0400

    First Commit

---
+++ Stack/Stack.cpp
@@ -0,0 +1,74 @@
+//
+// Created by ishkhan on 9/17/18.
+//
+
+#include <iostream>
+#include "Stack.h"
+
+Stack::Stack(int max_size) {
+    this->max_size = max_size;
+    m_location = new int[max_size];
+    m_top = -1;
+}
+
+Stack::Stack(const Stack &stack) {
+    m_top = stack.m_top;
+    max_size = stack.max_size;
+    m_location = new int[max_size];
+    for (int i = 0; i <= m_top; ++i) {
+        m_location[i] = stack.m_location[i];
+    }
+}
+
+Stack::~Stack() {
+    delete[] m_location;
+}
+
+Stack &Stack::operator=(Stack stack1) {
+    if (&stack1 != this) {
+        delete[] m_location;
+        new Stack(stack1);
+    }
+    return *this;
+}
+
+bool Stack::empty() const {
+    return size() == 0;
+}
+
+bool Stack::full() const {
+    return size() == max_size;
+}
+
+int Stack::size() const {
+    return m_top + 1;
+}
+
+int &Stack::top() {
+    return m_location[m_top];
+}
+
+void Stack::pop() {
+    if (!empty()) {
+        std::cout << m_location[m_top] << " popped from stack\n";
+        m_location[m_top--] = 0;
+    } else {
+        std::cout << "Stack is empty!\n";
+    }
+}
+
+void Stack::push(const int &x) {
+    if (!full()) {
+        std::cout << x << " pushed to stack\n";
+        m_location[++m_top] = x;
+    } else {
+        std::cout << "Stack is full!\n";
+    }
+}
+
+void Stack::display_stack() {
+    for (int i = 0; i <= m_top; ++i) {
+        std::cout << " | " << m_location[i];
+    }
+    std::cout << "\n";
+}
---
+++ Stack/Stack.h
@@ -0,0 +1,40 @@
+//
+// Created by ishkhan on 9/17/18.
+//
+
+#ifndef ALGORITHMS_IMPLEMETATION_STACK_H
+#define ALGORITHMS_IMPLEMETATION_STACK_H
+
+
+class Stack {
+private:
+    int max_size;
+    int m_top;
+    int *m_location;
+
+public:
+    Stack(int = 10);
+
+    Stack(const Stack &);
+
+    ~Stack();
+
+    Stack &operator=(Stack);
+
+    bool empty() const;
+
+    bool full() const;
+
+    int size() const;
+
+    int &top();
+
+    void pop();
+
+    void push(const int &);
+
+    void display_stack();
+};
+
+
+#endif //ALGORITHMS_IMPLEMETATION_STACK_H
