3fe54e7912d4f469f35f3ee1b82977b0562d690e
Ishkhan23 <32131681+Ishkhan23@users.noreply.github.com>
2018-09-27 11:01:36 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,17 +8,23 @@ int main() {
         queue->display_queue();
     }
     std::cout << queue->size() << " :size\n";
-    for (int j = 0; j < 5; ++j) {
+    for (int j = 0; j < 3; ++j) {
         queue->pop();
         queue->display_queue();
     }
     std::cout << queue->size() << " :size\n";
-    for (int i = 1; i <= 5; ++i) {
+    for (int i = 1; i <= 3; ++i) {
         queue->push(i);
         queue->display_queue();
     }
     std::cout << queue->size() << " :size\n";
     queue->display_queue();
     std::cout << queue->front() << "\n";
+	queue->display_queue();
+
+	std::cout << queue->m_front << " :m_front\n";
+	std::cout << queue->m_size << " :m_size\n";
+	std::cout << queue->m_rear << " :m_rear\n";
+	
     return 0;
 }
