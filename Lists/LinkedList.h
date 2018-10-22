30b32d371d304f2e572100558f4f5ce19721938c
Ishkhan23 <32131681+Ishkhan23@users.noreply.github.com>
2018-10-22 16:49:04 +0400

    Update LinkedList.h

--- Lists/LinkedList.h
+++ Lists/LinkedList.h
@@ -11,7 +11,29 @@ friend std::ostream &operator << (std::ostream &, const LinkedList &);
 public:
       LinkedList();
       ~LinkedLIst();
-      LinkedList LinkedList(const LinkedList &);
+      LinkedList(const LinkedList &);
+      LinkedList &operator=(const LinkedList &);
+      int &operator[](const int &);
+      const int &operator[](const int &);
       
-  
+      void push(const int &);
+      void push_at_index(const int &index, const int &);
+      void push_all(const int[] &);
+      void pop();
+      void remove_all(const int &);
+      void remove_at_index(const int &index);
+      void clear();
+      bool empty();
+      int size();
+      
+private:
+       struct Node{
+          Node* m_link;
+          int m_info;
+          Node(info)
+          : m_link(nullptr)
+          , m_info(info)
+          {}
+       }
+      int m_size;
 };
