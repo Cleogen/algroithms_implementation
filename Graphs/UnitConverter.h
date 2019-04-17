76b98ba677f53e61a89a5bde615733485b1cdc3b
Ishkhan23 <32131681+Ishkhan23@users.noreply.github.com>
2019-04-17 09:16:15 +0400

    Update UnitConverter.h

--- Graphs/UnitConverter.h
+++ Graphs/UnitConverter.h
@@ -7,17 +7,15 @@
 
 class UnitConverter {
 public:
-	UnitConverter();
+	void add_fact(const std::string &u1, const std::string &u2, const float &v1, const float &v2);
 
-	void add_fact(const std::string &u1, const std::string &u2, const float &v1, const double &v2);
+	float calculate(const std::string&, const std::string&, const float &) const;
 
-	float calculate(const std::string&, const std::string&, const double&) const;
-
-	std::map<std::pair<int, int>, float> m_relations;
 private:
 	std::vector<std::string> m_units;
+	std::map<std::pair<int, int>, float> m_relations;
 
-	const std::pair<int, int> get_or_create_ids(const std::string&, const std::string&);
+	std::pair<int, int> get_or_create_ids(const std::string&, const std::string&);
 	std::pair<int, int> get_ids(const std::string&, const std::string&) const;
 	float step(int, int) const;
 };
